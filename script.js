cb7d81971e5dd7505c0c84a4a4c46653e088dae2
solidados <geekbrains.pk@internet.ru>
2021-06-27 19:21:21 +0300

    proceed working on burger menu

---
+++ script.js
@@ -0,0 +1,18 @@
+// == HEADER & FOOTER SCRIPTS JQUERY =================
+$(function() {
+    $("#header").load("header.html");
+    $("#upper_footer").load("upper_footer.html")
+    $("#footer").load("footer.html");
+});
+
+// == MENU BURGER SCRIPT ==============================
+const menuBtn = document.querySelector('.header__item-right--menu');
+const menu = document.querySelector('.menu');
+const menuCloseBtn = document.querySelector('.menu__btn-close');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu-hidden');
+});
+menuCloseBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu-hidden');
+});
